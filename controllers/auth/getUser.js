60347211df7575c2259dee3a24f8fecb5c20f848
Pratik Majumdar (codadept) <info.pratikmajumdar@gmail.com>
2022-03-30 11:53:05 +0530

    FEAT: Some more auth routes add

---
+++ controllers/auth/getUser.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ */
+async function getUser(req, res) {
+  if (req.isAuthenticated()) {
+    res.json({ success: true, message: req.user });
+  } else {
+    res.json({
+      success: false,
+      error: "You are not authenticated. Please sign in.",
+    });
+  }
+}
+
+export { getUser };
